19dbaeda1a79435edec82bb2cc7eb341df8cf187
kangleng <kangleng@gmail.com>
2021-07-30 09:30:01 +0800

    Add function to print stars

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ Ensure you have a Github account
 def main():
     """Start the program"""
     print("Hello, My name is Bozo")
+    for i in range(1, 6):
+        print_stars(i)
+
+
+def print_stars(number):
+    print("*" * number)
 
 
 if __name__ == '__main__':
