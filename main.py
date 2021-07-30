20567c5b4fe32b32ac0cfa9760974df25e13d7ec
kangleng <kangleng@gmail.com>
2021-07-30 09:23:48 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Author: Bozo the Clown
+Date : 30 Jul 2021
+Description:
+This is a demo of Pycharm with Git and Github
+Ensure you have a Github account
+"""
+
+
+def main():
+    """Start the program"""
+    print("Hello, My name is Bozo")
+
+
+if __name__ == '__main__':
+    main()
